b14a5eb5b1b3def7d194f27dc1966bf1e6c6162f
Jackson Wallace <jackson.wallace@gmail.com>
2022-03-07 07:17:00 -0800

    delphes processor

--- tools/basic_objects.py
+++ tools/basic_objects.py
@@ -3,61 +3,106 @@ from tools.jmr import JMR
 
 #can add pt and eta cuts in later
 
-def getJets(events, corrector, pt_var='', scale_res = ''):
+def getJets(events, corrector, pt_var='', scale_res = '', delphes=False):
     """takes a set of events and a set of weights and
     weights the jet pts of those events according to
     the variation given."""
+    
+    if delphes == False:
+        jet_presel = (events.jetpuppi_pt>20)  # NOTE: careful with this presel, but needed to speed up the processor
 
-    jet_presel = (events.jetpuppi_pt>20)  # NOTE: careful with this presel, but needed to speed up the processor
+        jet = get_four_vec_fromPtEtaPhiM(
+                None,
+                pt = events.jetpuppi_pt[jet_presel],
+                eta = events.jetpuppi_eta[jet_presel],
+                phi = events.jetpuppi_phi[jet_presel],
+                M = events.jetpuppi_mass[jet_presel],
+                copy = False,
+            )
 
-    jet = get_four_vec_fromPtEtaPhiM(
-            None,
-            pt = events.jetpuppi_pt[jet_presel],
-            eta = events.jetpuppi_eta[jet_presel],
-            phi = events.jetpuppi_phi[jet_presel],
-            M = events.jetpuppi_mass[jet_presel],
-            copy = False,
-        )
-    
-    jet_pt_var = corrector.get(jet, pt_var)
-    
-    jet['pt'] = jet_pt_var[jet_presel]
-    
-    if scale_res != '':
-        jmr = JMR(seed=123)
-        jet = jmr.get(jet, scale=scale_res[0], res=scale_res[1])
-    
-    jet['id'] = events.jetpuppi_idpass[jet_presel]
-    jet['btag'] = events.jetpuppi_btag[jet_presel]
+        jet_pt_var = corrector.get(jet, pt_var)
+
+        jet['pt'] = jet_pt_var[jet_presel]
+
+        if scale_res != '':
+            jmr = JMR(seed=123)
+            jet = jmr.get(jet, scale=scale_res[0], res=scale_res[1])
+
+        jet['id'] = events.jetpuppi_idpass[jet_presel]
+        jet['btag'] = events.jetpuppi_btag[jet_presel]
+        
+    elif delphes == True:
+        jet_presel = (events.JetPUPPILoose.PT>20)  # NOTE: careful with this presel, but needed to speed up the processor
+
+        jet = get_four_vec_fromPtEtaPhiM(
+                None,
+                pt = events.JetPUPPILoose.PT[jet_presel],
+                eta = events.JetPUPPILoose.Eta[jet_presel],
+                phi = events.JetPUPPILoose.Phi[jet_presel],
+                M = events.JetPUPPILoose.Mass[jet_presel],
+                copy = False,
+            )
+
+        jet_pt_var = corrector.get(jet, pt_var)
+
+        jet['pt'] = jet_pt_var[jet_presel]
+
+        if scale_res != '':
+            jmr = JMR(seed=123)
+            jet = jmr.get(jet, scale=scale_res[0], res=scale_res[1])
     
     return jet
 
 
-def getFatjets(events, corrector, pt_var='', scale_res = ''):
+def getFatjets(events, corrector, pt_var='', scale_res = '', delphes=False):
     """takes a set of events and a set of weights and
     weights the fatjet pts of those events according to
     the variation given."""
     
-    fatjet = get_four_vec_fromPtEtaPhiM(
+    if delphes == False:
+        fatjet = get_four_vec_fromPtEtaPhiM(
+                None,
+                pt = events.fatjet_pt,
+                eta = events.fatjet_eta,
+                phi = events.fatjet_phi,
+                M = events.fatjet_msoftdrop,        #Using softdrop from now on
+                copy = False,
+            )
+
+        fatjet_pt_var = corrector.get(fatjet, pt_var)
+
+        fatjet['pt'] = fatjet_pt_var
+
+        if scale_res != '':
+            jmr = JMR(seed=123)
+            fatjet = jmr.get(fatjet, scale=scale_res[0], res=scale_res[1])
+
+        fatjet['tau1'] = events.fatjet_tau1
+        fatjet['tau2'] = events.fatjet_tau2
+        fatjet['tau3'] = events.fatjet_tau3
+        fatjet['tau4'] = events.fatjet_tau4
+        
+    elif delphes == True:
+        fatjet = get_four_vec_fromPtEtaPhiM(
             None,
-            pt = events.fatjet_pt,
-            eta = events.fatjet_eta,
-            phi = events.fatjet_phi,
-            M = events.fatjet_msoftdrop,        #Using softdrop from now on
+            pt = events.JetPUPPIAK8.PT,
+            eta = events.JetPUPPIAK8.Eta,
+            phi = events.JetPUPPIAK8.Phi,
+            M = events.JetPUPPIAK8.SoftDroppedJet.mass,        #Using softdrop from now on
             copy = False,
         )
-    
-    fatjet_pt_var = corrector.get(fatjet, pt_var)
-    
-    fatjet['pt'] = fatjet_pt_var
-    
-    if scale_res != '':
-        jmr = JMR(seed=123)
-        fatjet = jmr.get(fatjet, scale=scale_res[0], res=scale_res[1])
-    
-    fatjet['tau1'] = events.fatjet_tau1
-    fatjet['tau2'] = events.fatjet_tau2
-    fatjet['tau3'] = events.fatjet_tau3
-    fatjet['tau4'] = events.fatjet_tau4
+
+        fatjet_pt_var = corrector.get(fatjet, pt_var)
+
+        fatjet['pt'] = fatjet_pt_var
+
+        if scale_res != '':
+            jmr = JMR(seed=123)
+            fatjet = jmr.get(fatjet, scale=scale_res[0], res=scale_res[1])
+
+        fatjet['tau1'] = events.JetPUPPIAK8.Tau_5[:,:,0]
+        fatjet['tau2'] = events.JetPUPPIAK8.Tau_5[:,:,1]
+        fatjet['tau3'] = events.JetPUPPIAK8.Tau_5[:,:,2]
+        fatjet['tau4'] = events.JetPUPPIAK8.Tau_5[:,:,3]
     
     return fatjet
